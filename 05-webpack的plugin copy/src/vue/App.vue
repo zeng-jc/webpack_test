83249d28a94cbf92be6c5315c166aa5bfc61ec8b
VueTest <2356924146@qq.com>
2021-05-04 17:57:32 +0800

    main

---
+++ 05-webpack的plugin copy/src/vue/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2 class="title">{{ message }}</h2>
+    <button @click="btnClick">按钮</button>
+  </div>
+</template>
+
+<script>
+// npm install vue-loader vue-template-compiler --save-dev 解析vue文件的包
+export default {
+  name: "App",
+  data() {
+    return {
+      message: "hello webpack",
+    };
+  },
+  methods: {
+    btnClick() {
+      alert("2321231");
+    },
+  },
+};
+</script>
+
+<style>
+.title {
+  color: orange;
+}
+</style>
